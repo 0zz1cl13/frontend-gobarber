9be1ca894d5f97adbc4d59f941e921645fef1281
0zz1cl13 <seniorcl13@gmail.com>
2020-03-16 13:30:47 -0400

    feat: add login do usuario prestador de serviço

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
 
+import { signInRequest } from '~/store/modules/auth/actions';
+
 export default function SignIn() {
+  const dispatch = useDispatch();
+
   const schema = Yup.object().shape({
     email: Yup.string()
       .email('Insira um e-mail válido')
@@ -13,8 +18,8 @@ export default function SignIn() {
     password: Yup.string().required('A senha é obrigatória'),
   });
 
-  function handleSubmit(data) {
-    console.tron.log(data);
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
   }
 
   return (
