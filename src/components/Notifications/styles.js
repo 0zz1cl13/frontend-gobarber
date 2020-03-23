1c4e629347e1b25a715b510e6b143eb9687be9cb
0zz1cl13 <seniorcl13@gmail.com>
2020-03-23 16:41:16 -0400

    feat(notifications): add funcionalidade de notificaçoes de agendamento

--- src/components/Notifications/styles.js
+++ src/components/Notifications/styles.js
@@ -36,6 +36,8 @@ export const NotificationList = styled.div`
   border-radius: 4px;
   padding: 15px 5px;
 
+  display: ${props => (props.visible ? 'block' : 'none')};
+
   &::before {
     content: '';
     position: absolute;
@@ -64,8 +66,10 @@ export const Notification = styled.div`
   }
 
   time {
+    display: block;
     font-size: 12px;
     opacity: 0.6;
+    margin-bottom: 5px;
   }
 
   button {
@@ -73,9 +77,9 @@ export const Notification = styled.div`
     border: 0;
     background: none;
     color: ${lighten(0.2, '#7159d1')};
-    padding: 0 5px;
+    /* padding: 0 5px;
     margin: 0 5px;
-    border-left: 1px solid rgba(255, 255, 255, 0.1);
+    border-left: 1px solid rgba(255, 255, 255, 0.1); */
   }
 
   ${props =>
@@ -88,6 +92,7 @@ export const Notification = styled.div`
         height: 8px;
         background: #ff892e;
         border-radius: 50%;
+        margin-left: 10px;
       }
     `}
 `;
