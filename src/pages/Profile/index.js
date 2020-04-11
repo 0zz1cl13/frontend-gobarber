60e531e3949a6c5e4d62172bcb08a902a9e12d74
0zz1cl13 <seniorcl13@gmail.com>
2020-04-11 01:43:49 -0400

    feat(profile): add estilizacao profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" type="text" placeholder="Seu nome" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+
+        <hr />
+
+        <Input name="oldPassowrd" type="passowrd" placeholder="Senha atual" />
+        <Input name="passowrd" type="passowrd" placeholder="Nova senha" />
+        <Input
+          name="confirmPassowrd"
+          type="passowrd"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">Atualizar perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
