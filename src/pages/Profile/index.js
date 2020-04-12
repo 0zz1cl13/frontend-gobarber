1ffc221ddb6a51b35a4403394823c9bcc960d317
0zz1cl13 <seniorcl13@gmail.com>
2020-04-11 21:30:10 -0400

    feat(profile/avatar): add update do avatar do user

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 
 import { Form, Input } from '@rocketseat/unform';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
+import AvatarInput from './AvatarInput';
 import { Container } from './styles';
 
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
 export default function Profile() {
+  const dispatch = useDispatch();
+
   const profile = useSelector(state => state.user.profile);
 
-  function handleSubmit(data) {}
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
 
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
+
         <Input name="name" type="text" placeholder="Seu nome" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
 
--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,3 +1,28 @@
-import { all } from 'redux-saga/effects';
+import { put, call, all, takeLatest } from 'redux-saga/effects';
 
-export default all([]);
+import { toast } from 'react-toastify';
+import api from '~/services/api';
+
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, avatar_id, ...rest } = payload.data;
+
+    const profile = {
+      name,
+      email,
+      avatar_id,
+      ...(rest.oldPassowrd ? rest : {}),
+    };
+
+    const response = yield call(api.put, '/users', profile);
+    toast.success('Perfil atualizado com sucesso!');
+    yield put(updateProfileSuccess(response.data));
+  } catch (e) {
+    toast.error('Erro na atualização, verifique seus dados!');
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
